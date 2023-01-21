9c603ab4a39581ab9b24335ea07183756c9878ac
Oguzhan Ipek <fgtyhtyh@hotmail.com>
2023-01-22 00:24:43 +0300

    add react-query client and ky for api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
 import { useScrollLock } from '@mantine/hooks';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SpotlightProvider } from '@mantine/spotlight';
 import { IconSearch } from '@tabler/icons';
 import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/router';
 import { SpotlightActions } from './stores/SpotlightActions';
 import { AuthProvider } from './util/AuthProvider';
 
+const queryClient = new QueryClient();
 export default function App() {
   const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
   const [_scrollock] = useScrollLock(true);
@@ -15,20 +18,23 @@ export default function App() {
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
   return (
-    <AuthProvider>
-      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-        <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
-          <SpotlightProvider
-            shortcut={['mod + K']}
-            actions={SpotlightActions}
-            searchIcon={<IconSearch size={18} />}
-            searchPlaceholder="Search..."
-            nothingFoundMessage="Nothing found..."
-          >
-            <RouterProvider router={router} />
-          </SpotlightProvider>
-        </MantineProvider>
-      </ColorSchemeProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+          <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+            <SpotlightProvider
+              shortcut={['mod + K']}
+              actions={SpotlightActions}
+              searchIcon={<IconSearch size={18} />}
+              searchPlaceholder="Search..."
+              nothingFoundMessage="Nothing found..."
+            >
+              <RouterProvider router={router} />
+              <ReactQueryDevtools initialIsOpen={false} />
+            </SpotlightProvider>
+          </MantineProvider>
+        </ColorSchemeProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
