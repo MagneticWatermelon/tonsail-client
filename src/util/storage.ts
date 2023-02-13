db951ebe28dcd99fbde6d4015482d37a5fed1eb3
Oguzhan Ipek <fgtyhtyh@hotmail.com>
2023-02-13 21:32:32 +0300

    change setSession hook argument type to User

--- src/util/storage.ts
+++ src/util/storage.ts
@@ -1,15 +1,17 @@
+import { User } from '../types/User';
+
 const storagePrefix = 'tonsail_app_';
 
 const storage = {
   getSession: () => {
     return JSON.parse(window.localStorage.getItem(`${storagePrefix}session`) as string);
   },
-  setSession: (token: string) => {
-    window.localStorage.setItem(`${storagePrefix}session`, JSON.stringify(token));
+  setSession: (user: User) => {
+    window.localStorage.setItem(`${storagePrefix}session`, JSON.stringify(user));
   },
   clearSession: () => {
     window.localStorage.removeItem(`${storagePrefix}session`);
-  },
+  }
 };
 
 export default storage;
