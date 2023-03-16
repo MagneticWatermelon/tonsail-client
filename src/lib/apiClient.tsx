ba66f9f724a0d9989336a1e081da85e719aae7bd
Oguzhan Ipek <fgtyhtyh@hotmail.com>
2023-03-16 17:43:39 +0300

    remove beforeError hook from ky client

--- src/lib/apiClient.tsx
+++ src/lib/apiClient.tsx
@@ -1,28 +1,8 @@
-import { showNotification } from '@mantine/notifications';
-import { IconX } from '@tabler/icons-react';
-import ky, { HTTPError } from 'ky';
+import ky from 'ky';
 import { BASE_API_URL } from '../config';
-
-const handleErrors = async (error: HTTPError) => {
-  if (error.response.status === 401) {
-    return error;
-  }
-  let errorBody = await error.response.json();
-  showNotification({
-    title: errorBody.title,
-    message: errorBody.message,
-    autoClose: 5000,
-    color: 'red',
-    icon: <IconX />
-  });
-  return error;
-};
 
 export const client = ky.create({
   prefixUrl: BASE_API_URL,
   mode: 'cors',
-  credentials: 'include',
-  hooks: {
-    beforeError: [handleErrors]
-  }
+  credentials: 'include'
 });
