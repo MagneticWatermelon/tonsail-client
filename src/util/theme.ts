1885747c4c16a1d94df4e5a15fc587a6ddc350d7
Oguzhan Ipek <fgtyhtyh@hotmail.com>
2023-04-10 16:07:18 +0300

    prep for run analytics dashboard

--- src/util/theme.ts
+++ src/util/theme.ts
@@ -17,6 +17,22 @@ declare module '@mantine/core' {
   }
 }
 export const theme: MantineThemeOverride = {
+  globalStyles: (theme) => ({
+    '*, *::before, *::after': {
+      boxSizing: 'border-box'
+    },
+
+    body: {
+      ...theme.fn.fontStyles(),
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+      lineHeight: theme.lineHeight
+    },
+
+    'svg:not(:root)': {
+      overflow: 'unset'
+    }
+  }),
   components: {
     Input: {
       styles: (theme) => ({
